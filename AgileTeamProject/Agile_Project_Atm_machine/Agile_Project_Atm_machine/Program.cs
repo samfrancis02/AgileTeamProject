f854b431d5d981bb66a3aa3984a98cf7eb929ff2
OmerHakanKilic <156901186+OmerHakanKilic@users.noreply.github.com>
2025-01-22 12:58:18 +0000

    Add files via upload

---
+++ AgileTeamProject/Agile_Project_Atm_machine/Agile_Project_Atm_machine/Program.cs
@@ -0,0 +1,17 @@
+namespace Agile_Project_Atm_machine
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
